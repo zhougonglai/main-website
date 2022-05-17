3d78eae49aac9f15a7bba7f37a67a2c66d189b8a
周公来 <zhougonglai@nn.com>
2022-05-17 16:32:45 +0800

    feat: 产品页面

---
+++ components/Checkbox/index.vue
@@ -0,0 +1,261 @@
+<template lang="pug">
+.app-checkbox
+  .checkbox-con
+    input.checkbox(
+      type="checkbox"
+      :value="value"
+      :disabled="disabled"
+      @input="e => $emit('update:value', e.target.value)"
+      @change="e => $emit('change', e.target.checked)")
+    .checkbox-mask
+      i.check
+        span
+          .line1
+          .line2
+  slot
+</template>
+<script>
+export default {
+  name: 'app-checkbox',
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-checkbox {
+  font-family: Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  clear: left;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  .checkbox-con {
+    width: 23px;
+    height: 23px;
+    border-radius: 9px;
+    position: relative;
+    z-index: 1;
+  }
+
+  input[type="checkbox"] {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0px;
+    left: 0px;
+    margin: 0px;
+    padding: 0px;
+    opacity: 0;
+    z-index: 100;
+    cursor: pointer;
+
+    &:disabled {
+      opacity: 0;
+      pointer-events: none;
+
+      ~.checkbox-mask {
+        cursor: not-allowed;
+        opacity: .6;
+        background: transparent !important;
+      }
+    }
+
+    &:active {
+      ~.checkbox-mask {
+        background-color: gray;
+      }
+    }
+
+    &:hover {
+      ~.checkbox-mask {
+        background-color: lightgray;
+
+        &:before {
+          border: 2px solid darkgray;
+        }
+      }
+    }
+
+    &:checked {
+      &:hover {
+        ~.checkbox-mask {
+          box-shadow: 0px 3px 15px 0px var(--primary);
+        }
+      }
+
+      ~.checkbox-mask {
+        box-shadow: 0px 3px 15px 0px var(--primary);
+
+        i:not(.check) {
+          opacity: 1;
+          transform: scale(1);
+          transition: all .25s ease .15s;
+        }
+
+        .check {
+          opacity: 1;
+
+          span {
+            .line1 {
+              &:after {
+                width: 100%;
+                transition: all .25s ease .1s;
+              }
+            }
+
+            .line2 {
+              &:after {
+                transition: all .2s ease .30s;
+                height: 100%;
+              }
+            }
+          }
+        }
+
+        &:after {
+          opacity: 1;
+          transform: scale(1);
+        }
+
+        &:before {
+          opacity: 0;
+          transform: scale(1.2);
+        }
+      }
+    }
+  }
+
+  .checkbox-mask {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 50;
+    cursor: pointer;
+    pointer-events: none;
+    box-sizing: border-box;
+    transition: all .25s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: -1;
+    box-sizing: border-box;
+
+    i:not(.check) {
+      opacity: 0;
+      transition: all .25s ease;
+      color: #fff;
+      font-size: 1.1em;
+      transform: scale(.5);
+    }
+
+    .check {
+      opacity: 0;
+      z-index: 200;
+      width: 23px;
+      height: 23px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      transition: all .25s ease;
+      border-radius: inherit;
+
+      span {
+        transform: rotate(45deg);
+        display: block;
+        position: relative;
+        width: 8px;
+        height: 13px;
+        margin-top: -4px;
+
+        .line1 {
+          background: transparent;
+          content: '';
+          position: absolute;
+          height: 2px;
+          border-radius: 2px;
+          transition: all .2s ease;
+          border-radius: 5px;
+          z-index: 100;
+          width: 8px;
+          bottom: 0px;
+
+          &:after {
+            content: '';
+            position: absolute;
+            width: 0%;
+            height: 2px;
+            background: #fff;
+            transition: all .25s ease;
+            border-radius: 5px 0px 0px 5px;
+          }
+        }
+
+        .line2 {
+          bottom: 0px;
+          right: 0rem;
+          z-index: 100;
+          border-radius: 5px;
+          background: transparent;
+          content: '';
+          position: absolute;
+          height: 13px;
+          border-radius: 5px;
+          transition: all .2s ease;
+          width: 2px;
+
+          &:after {
+            content: '';
+            position: absolute;
+            width: 2px;
+            height: 0%;
+            background: #fff;
+            transition: all .25s ease;
+            bottom: 0px;
+            border-radius: 5px 5px 0px 0px;
+          }
+        }
+      }
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      height: 100%;
+      background: var(--primary);
+      transform: scale(.5);
+      border-radius: inherit;
+      opacity: 0;
+      transition: all .25s ease;
+      z-index: -1;
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      height: 100%;
+      border-radius: inherit;
+      transition: all .25s ease;
+      z-index: -1;
+      border: 2px solid gray;
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
--- pages/production.vue
+++ pages/production.vue
@@ -13,44 +13,61 @@ section.w-full.flex.flex-col
         p.my-5 创远仪器提供广泛的多功能、高性能网络分析仪（最高26.5GHz）和标准多端口解决方案。创远仪器的矢量网络分析仪非常适用于分析无源及有源器件，比如滤波器、放大器、混频器及多端口模块。
         p 网络分析仪具有出色的射频特性以及丰富的分析功能，有助于用户快速评估重要参数。
   .sc-2.flex.items-center.justify-center
-    .sc-2__block.py-10
-      .prod.border.border-gray-200
-        .prod-header.flex.p-5.flex.items-center.justify-between.text-blue-500
-          .prod-title.flex-1.text-2xl.leading-relaxed 产品清单
-          .prod-icon
-            ion-icon.text-4xl(name="remove-outline")
-        .prod-thd.flex.p-5
-          .prod-td.flex-1 对比
-          .prod-td.flex-1 产品名称
-          .prod-td.flex-1 产品形态
-          .prod-td.flex-1 最大频率
-          .prod-td.flex-1 端口数
-          .prod-td.flex-1 型号
-          .prod-td.flex-1
-        .prod-tbd
-          .prod-tl.border-top.border-gray-200.h-20.flex.items-center(v-for="(prod, i) in productions" :key="i")
-            .prod-ti.flex-1
-              input(type="checkbox" v-model="selected[prod.id]")
-              img.object-center.object-cover(:src="prod.url")
-            .prod-ti.flex-1
+    .sc-2__block.py-20
+      table.prod.border-collapse.border-gray-200.border.table-fixed.w-full.relative
+        thead.prod-header
+          tr.flex.p-5.flex.items-center.justify-between.text-blue-500
+            .prod-title.flex-1.text-2xl.leading-relaxed 产品清单
+            .prod-icon
+              ion-icon.text-4xl(name="remove-outline")
+        thead.prod-thd
+          tr.flex.leading-relax.border-gray-200.border
+            th.prod-td.flex-1.p-5 对比
+            th.prod-td.flex-1.p-5 产品名称
+            th.prod-td.flex-1.p-5 产品形态
+            th.prod-td.flex-1.p-5 最大频率
+            th.prod-td.flex-1.p-5 端口数
+            th.prod-td.flex-1.p-5 型号
+            th.prod-td.flex-1.p-5
+        tbody.prod-tbd
+          tr.prod-tl.border-top.border-gray-200.h-20.flex.items-center(v-for="(prod, i) in productions" :key="i")
+            td.prod-ti.flex-1.p-5
+              //- input(type="checkbox" v-model="selected[prod.id]")
+              Checkbox(@change="selectChange($event, prod.id)" :disabled="selected[prod.id] ? false : selectedLength >= 3")
+                img.ml-5.object-center.object-cover(:src="prod.url")
+            td.prod-ti.flex-1.p-5
               button(v-text="prod.title")
-            .prod-ti.flex-1(v-text="prod.type")
-            .prod-ti.flex-1(v-text="prod.qz")
-            .prod-ti.flex-1(v-text="prod.port")
-            .prod-ti.flex-1(v-text="prod.types.join('/')")
-            .prod-ti.flex-1
-              button 询价
+            td.prod-ti.flex-1.p-5.text-center(v-text="prod.type")
+            td.prod-ti.flex-1.p-5.text-center(v-text="prod.qz")
+            td.prod-ti.flex-1.p-5.text-center(v-text="prod.port")
+            td.prod-ti.flex-1.p-5(v-text="prod.types.join('/')")
+            td.prod-ti.flex-1.p-5.flex.items-center.justify-center
+              button.bg-blue-500.px-5.py-2.text-white 询价
+        tfoot.prod-tfd.absolute.w-full.flex.items-center.justify-center
+          button.bg-blue-500.px-5.py-2.text-white.-mt-5(:disabled="selectedLength < 2") {{ selectedLength }}/3 产品比较
 </template>
 
 <script>
 import productions from '@/assets/constant/production.json';
+// import Checkbox from '~/components/Checkbox/index.vue';
 
 export default {
   name: "production-page",
   data() {
     return {
       productions,
-      selected: {}
+      selected: {},
+    };
+  },
+  computed: {
+    selectedLength() {
+      return Object.values(this.selected).filter(v => v).length
+    }
+  },
+  methods: {
+    selectChange(checked, id) {
+      console.log('checked', checked, id);
+      this.$set(this.selected, id, checked)
     }
   }
 }
@@ -74,8 +91,16 @@ export default {
 .prod {
   &-ti {
     img {
-      max-width: 200px;
-      max-height: 150px;
+      max-width: 100px;
+      max-height: 100px;
+    }
+  }
+
+  &-tfd {
+    button {
+      &:disabled {
+        filter: grayscale(1);
+      }
     }
   }
 }
