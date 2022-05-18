8c19a791cf7da4bee17a6e752da60f8373d5df42
周公来 <zhougonglai@nn.com>
2022-05-18 17:18:15 +0800

    feat: 产品详情页面

---
+++ components/Dialog/index.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+dialog.grid.round.shadow(:open="open")
+  slot
+</template>
+<script>
+export default {
+  name: "app-dialog"
+}
+</script>
+<style lang="scss" scoped>
+dialog {
+  &:not([open]) {
+    pointer-events: none;
+    opacity: 0;
+  }
+
+  background: white;
+  color: black;
+  max-inline-size: 90vw;
+  max-block-size: min(80vh, 100%);
+  max-block-size: min(80dvb, 100%);
+  overflow: hidden;
+  margin: auto;
+  padding: 0;
+  position: fixed;
+  inset: 0;
+  z-index: 9;
+
+  &::backdrop {
+    backdrop-filter: blur(25px);
+  }
+}
+
+@media (max-width: 768px) {
+  dialog[modal-mode="mega"] {
+    margin-block-end: 0;
+    border-end-end-radius: 0;
+    border-end-start-radius: 0;
+  }
+}
+</style>
--- pages/production/t526c.vue
+++ pages/production/t526c.vue
@@ -6,9 +6,9 @@ section.w-full.flex.flex-col.items-center.justify-center
     ion-icon.mx-5(name="chevron-forward-sharp"  v-cloak)
     nuxt-link(to="/production") 矢量网络分析
     ion-icon.mx-5(name="chevron-forward-sharp"  v-cloak)
-    .nav-menus.flex.items-center.relative
-      .nav-menu(@click="show = !show") T5260C 485矢量网络分析仪
-      ion-icon.ml-5(name="chevron-down-sharp"  v-cloak @click="show = !show")
+    .nav-menus.flex.items-center.relative.cursor-pointer(ref="menus")
+      .nav-menu(@click.stop="show = !show") T5260C 485矢量网络分析仪
+        ion-icon.ml-5(name="chevron-down-sharp"  v-cloak)
       transition(
         enter-active-class="transition duration-100 ease-out"
         enter-from-class="transform scale-95 opacity-0"
@@ -16,9 +16,26 @@ section.w-full.flex.flex-col.items-center.justify-center
         leave-active-class="transition duration-75 ease-in"
         leave-from-class="transform scale-100 opacity-100"
         leave-to-class="transform scale-95 opacity-0")
-        ul.nav-drops.absolute(v-if="show")
-          li.nav-link(v-for="(prod, i) in productions" :key="i")
-            nuxt-link(to="/production/t526c") {{ prod.title }}
+        ul.nav-drops.absolute.bg-white.top-full.w-full.py-2.shadow(v-if="show")
+          li.nav-link.pl-4(v-for="(prod, i) in productions" :key="i" class="hover:bg-gray-300")
+            nuxt-link.truncate(to="/production/t526c") {{ prod.title }}
+  .sc-1.w-full.flex.flex-col.items-center.py-10
+    .sc-1__block
+      h1.text-2xl T5260C系列矢量网络分析仪
+      .flex.w-full.my-5.gap-x-5
+        .flex.flex-col.flex-1.relative
+          img.object-center.object-cover.overflow-hidden(src="https://img01.yzcdn.cn/vant/cat.jpeg" width="600" height="400")
+          ion-icon.absolute.right-0.bottom-0.text-blue-500(name="expand-sharp" size="large")
+        .flex.flex-col.flex-1
+          h3.text-2xl 关键特性
+          ul.mt-5
+            li 频率范围：300kHz~8.5GHz
+            li 大动态范围：＞ 125 dB @(IFBW=10 Hz )，typ.130 dB
+            li 低迹线噪声：2 mdB rms @(IFBW=3 kHz )
+            li 测量速度快：42 μs/point @(IFBW=500 kHz)
+            li 高等效方向性：＞ 45 dB
+            li 支持远程控制： LAN
+
 
 </template>
 <script>
@@ -28,15 +45,28 @@ export default {
   name: 'production-detail-page',
   data() {
     return {
-      productions,
+      productions: productions.filter((p, i) => i),
       show: false
     }
   },
+  mounted() {
+    window.addEventListener('click', e => {
+      if (!this.$refs?.menus?.contains(e.target)) {
+        this.show = false
+      }
+    })
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 nav {
   width: 1200px;
+}
+
+.sc-1 {
+  &__block {
+    width: 1200px;
+  }
 }
 </style>
