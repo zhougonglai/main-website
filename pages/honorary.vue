68b133042c6aeb48522b713fe612bf46632619cc
zhang-zk <zhangzongkuo@nn.com>
2022-05-18 09:23:49 +0800

    feat: 荣誉资质

--- pages/honorary.vue
+++ pages/honorary.vue
@@ -9,13 +9,32 @@
       ion-icon(name='chevron-forward-outline') 
       | 荣誉资质
   .banner.w-full
-  section.container
-    | 荣誉资质
+  section.container.desc
+    .content 公司自2005年成立以来，一直围绕技术创新和市场开拓优化知识产权布局，力争做到知识产权具有数量规模、质量优势的资源储备，有效支撑企业不断创新发展。
+    hr
+  section.w-full.flex.flex-col.items-center.desc2
+    .container
+      h2 国际权威机构测试认证
+      Carousel(:items="banner")
+  section.w-full.flex.flex-col.items-center.desc2
+    .container
+      h2 我们拥有140项已授权行业专利
+      Carousel(:items="banner")
+  section.w-full.flex.flex-col.items-center.desc2
+    .container
+      h2 获得奖项
+      Carousel(:items="banner")
 </template>
 <script>
+import banner from "@/assets/constant/banner.json";
 export default {
-
-}
+  name: "about-honorary",
+  data() {
+    return {
+      banner,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -26,10 +45,32 @@ nav.Bread-crumbs {
   color: #606060;
 }
 .banner {
-  height: 438px;
+  height: 484px;
   background: url("../assets/img/bg_sunset.jpg") no-repeat center / cover;
 }
 .container {
   width: 1200px;
+}
+.desc {
+  width: 1047px;
+  hr {
+    border-bottom: 2px solid #666666;
+    margin: 30px 0;
+  }
+  .content {
+    margin-top: 43px;
+    font-size: 24px;
+    color: #585858;
+    line-height: 42px;
+  }
+}
+.desc2 {
+  .container {
+    width: 1047px;
+  }
+  h2 {
+    font-size: 48px;
+    color: #222222;
+  }
 }
 </style>
