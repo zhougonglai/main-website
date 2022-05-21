515bf7b46bfa55a417a6bae80ad0198bb4db5009
周公来 <zhougonglai@nn.com>
2022-05-22 01:34:31 +0800

    feat: 更新新闻导航

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -7,7 +7,7 @@
       br
       | 快速发展的测试技术以及创远为国产仪器所作出的卓越贡献。
   .new-list
-    a.new(v-for="db,i in 16" :key="i" :href="'news/_id?id='+i") 
+    a.new(v-for="db, i in 16" :key="i" :href="'news/_id?id=' + i")
       .img
       .title
         h5 创远仪器参加2021年mwc
@@ -17,7 +17,7 @@
 </template>
 <script>
 export default {
-
+  name: "news-page"
 };
 </script>
 <style lang="scss" scoped>
@@ -26,30 +26,37 @@ export default {
   background: url("../../assets/img/bg_sunset.jpg") no-repeat center / cover;
   margin-bottom: 136px;
   line-height: 1;
+
   .box {
     width: 770px;
   }
+
   h1 {
     font-size: 36px;
     color: #262626;
     margin: 82px 0 31px;
   }
 }
+
 .new-list {
   width: 1482px;
   margin: 0 -13px 100px;
+
   .new {
     display: inline-block;
     width: 468px;
     margin: 24px 13px;
+
     .img {
       height: 246px;
       background: url("../../assets/img/bg_sunset.jpg") no-repeat center / cover;
     }
+
     .title {
       padding: 20px;
       height: 188px;
       background: #efefef;
+
       h5 {
         margin-top: 10px;
         font-size: 24px;
@@ -58,16 +65,18 @@ export default {
         overflow: hidden;
         text-overflow: ellipsis;
       }
+
       .desc {
         justify-content: space-between;
         margin-top: 50px;
         font-size: 24px;
         color: #111111;
+
         .link {
           border-bottom: 1px solid #383838;
         }
-        .time {
-        }
+
+        .time {}
       }
     }
   }
