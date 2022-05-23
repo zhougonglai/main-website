e6aad1f7f10b39ba9a04ae2bab7ef54ded2ad054
周公来 <zhougonglai@nn.com>
2022-05-23 18:12:05 +0800

    style: fix

--- pages/service/index.vue
+++ pages/service/index.vue
@@ -7,7 +7,7 @@
     <div class="sp-1 flex items-center justify-center">
       <div class="sp-1__block py-20">
         <h1 class="text-4xl font-bold">无线电监测</h1>
-        <article class="mt-5">
+        <article class="leading-8 mt-5">
           随着国家经济发展，无线电频谱资源的使用也越来越多，特别是 5G 的商用，使得频谱资源从原来的 3GHz 以下提升到 6GHz
           以下。国家无线电管理局也出台了新的无线电监测固定站的建设标准。随着新的标准执行，原有的旧有站点在监测频率范围、测向频率范围、信号分析、特殊信号解调、原子化服务、动环控制等领域进行升级改造后才能适应当下频谱管理的需求。
         </article>
