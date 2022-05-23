46409538fb37520cf0b2c99026b03455c88bc5c5
zzk <914066828@qq.com>
2022-05-23 21:29:11 +0800

    feat: 关于我们-联系我们

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 .w-full.flex.flex-col.items-center
   .banner.w-full
+    h1 联系我们
   .container.desc
     p 上海创远仪器技术股份有限公司（简称创远仪器）成立于 2005 年，总部位于上海，首批北交所上市（831961），是一家自主研发射频通信测试仪器和提供整体测试解决方案的专业仪器仪表公司。专注于无线通信、射频微波、无线电监测、北斗导航及智能制造测试领域，拥有自主品牌和一系列测试仪器核心专利技术，集研发、生产和销售为一体，是一家具有自主知识产权的高新技术企业。荣获 2016 年度国家科学技术进步奖特等奖和 2020 年度江苏省科学技术奖一等奖。
     p 创远仪器多年承担国家“新一代宽带无线移动通信网”科技重大专项课题开发任务及上海市高新技术产业化专项、战略性新兴产业发展专项、科技创新行动等研发任务，承担上海无线通信测试仪器工程技术研究中心的持续建设任务，全面展开5G/6G 通信测试技术的研究，参与国家 5G/6G 测试规范及标准制定，是国家知识产权优势企业，上海市专利工作示范企业，截止到 2021 年底累计申请国内外专利364 个。2020 年联合东南大学等单位发布了 5G 毫米波技术白皮书。
@@ -59,14 +60,16 @@
       input(v-model="db.name")
     .flex-1
       label 城市
-      input(v-model="db.company")
+      select(v-model="db.company" placeholder="请选择")
    .field.flex
     .flex-1
       label 电话号码
       input(v-model="db.name")
     .flex-1
       label 询问
-      input(v-model="db.company")
+      select(v-model="db.question" placeholder="请选择")
+        option 想具体了解产品报价及指标
+        option 需要售后指导及维修方面问询
    .field
     textarea(v-model="db.company" placeholder="具体问题描述")
 </template>
@@ -79,6 +82,7 @@ export default {
         sex: 1,
         name: "",
         company: "",
+        question: "",
       },
     };
   },
@@ -96,57 +100,57 @@ $blue: #005fab;
 }
 .desc {
   width: 1123px;
-  font-size: 28px;
+  font-size: 26px;
   color: #464646;
-  line-height: 48px;
-  margin: 114px 0 128px;
+  line-height: 45px;
+  margin: 90px 0 100px;
   p {
-    margin-top: 30px;
+    margin-top: 28px;
   }
 }
 .contact {
-  width: 1560px;
+  width: 1248px;
   background: #f5f5f5;
   border-radius: 6px;
-  padding: 98px 70px;
+  padding: 78px 56px;
   .item {
-    padding: 23px 33px;
-    font-size: 23px;
-    margin: 17px;
+    padding: 18px 26px;
+    font-size: 18px;
+    margin: 13px;
     color: #525252;
-    line-height: 50px;
+    line-height: 40px;
     background: #fff;
     .title {
-      font-size: 40px;
+      font-size: 32px;
       font-weight: bold;
       color: #6d6d6d;
-      line-height: 36px;
-      margin: 40px 0;
+      line-height: 28px;
+      margin: 32px 0;
     }
     b {
       margin: 4px 0 14px;
     }
   }
   .item1 {
-    padding: 38px 40px;
-    font-size: 28px;
-    line-height: 36px;
+    padding: 30px 32px;
+    font-size: 22px;
+    line-height: 28px;
     .title {
-      margin: 60px 0;
+      margin: 48px 0;
     }
   }
 }
 .form {
-  width: 1340px;
-  padding: 85px 0;
-  font-size: 26px;
+  width: 1072px;
+  padding: 68px 0;
+  font-size: 20px;
   color: #252525;
   h2 {
-    font-size: 40px;
+    font-size: 32px;
   }
   > p {
-    margin-top: 40px;
-    font-size: 26px;
+    margin-top: 32px;
+    font-size: 20px;
     color: #626363;
   }
   .radio {
@@ -166,6 +170,7 @@ $blue: #005fab;
       color: #252525;
       margin: 25px 0;
     }
+    select,
     input {
       width: 499px;
       height: 50px;
@@ -175,10 +180,11 @@ $blue: #005fab;
     }
     textarea {
       margin-top: 20px;
-      width: 100%;
+      width: 1035px;
       height: 283px;
       background: #eeeeed;
       border: 1px solid #303131;
+      padding: 20px 25px;
     }
   }
 }
