80d3fd50a4690c2cf47a56b3490b4566242f0229
周公来 <zhougonglai@nn.com>
2022-07-20 10:34:18 +0800

    feat

--- pages/apply/summary/_ab_id.vue
+++ pages/apply/summary/_ab_id.vue
@@ -2,7 +2,7 @@
 section.w-full
   .sc-1.flex.items-center.justify-center.relative
     img.absolute.inset-0.object-center.object-cover.h-full.z-0(:src="basePath + summary.cover_path" width="100%" height="100%")
-    .sc-1__block.color-white.z-1.py-32(class="sm:px-2 md:px-4 lg:px-6 xl:px-8 px-10")
+    .sc-1__block.color-white.z-1.py-32.container(class="sm:px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64")
       h1.text-4xl.text-white(v-text="summary.name")
       article.mt-5
         p.text-white.text-2xl.leading-relaxed(v-text='summary.content')
@@ -15,12 +15,12 @@ section.w-full
     .sc-3__bg.bg-gray-100.absolute.inset-x-0.bottom-0.top-80.z-0
     .sc-3__block.py-20.container
       h1.text-4xl 相关应用
-      .grid.grid-cols-3.gap-5.mt-10
+      .grid.gap-5.mt-10(class="lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2")
         nuxt-link.card.flex.flex-col.cursor-pointer.bg-white.border-b-2.border-white(
           class="hover:shadow hover:border-blue-300" v-for="(apply, i) in summary.applys" :key="apply.id"
           :to="`/apply/${apply.id}`")
           .card-cover
-            img.object-center.object-cover(:src="basePath + apply.cover_path" width="387" height="261")
+            img.object-center.object-cover.w-full(:src="basePath + apply.cover_path")
           .card-content.p-5
             h3.text-2xl.card-title(v-text="apply.name")
             p.mt-3.card-desc.text-gray-500.leading-8(v-text="apply.title")
@@ -29,20 +29,20 @@ section.w-full
               ion-icon.mr-2(name="arrow-forward-outline")
               | 更 多 信 息
   .sc-4.flex.items-center.justify-center
-    .sc-4__block.flex.py-20.container
+    .sc-4__block.flex.py-20.container(class="flex-col lg:flex-row")
       .left.flex-1
         h1.text-4xl(v-text="summary.video_title")
         p.mt-5.text-2xl(v-text="summary.video_content")
       .right.flex-1
         video(:src="basePath + summary.goods_video" controls)
   .sc-5.flex.items-center.justify-center.bg-gray-100
-    .sc-5__block.flex.flex-col.py-20
+    .sc-5__block.flex.flex-col.py-20.container
       h1.text-4xl 推荐产品
-      carousel-card.mt-10(size="3" v-if="summary.products" :max-size="summary.products.length" content-class="flex overflow-hidden content")
-        carousel-item(v-for="(p, i) in summary.products" :key="p.id" class="item flex-shrink-0")
+      carousel-card.mt-10(size="4" v-if="summary.products" :max-size="summary.products.length" content-class="flex overflow-hidden content space-x-8 px-4 md:px-0")
+        carousel-item(v-for="(p, i) in summary.products" :key="p.id" class="item w-full lg:w-80 flex-shrink-0")
           nuxt-link.card.flex.flex-col.bg-white.border-b-2.border-white.h-full(class="hover:shadow hover:border-blue-300" :to="`/prod/${p.category_id}/detail/${p.id}`")
             .card-cover
-              img.object-center.object-contain.h-40(:src="basePath + p.cover_path")
+              img.object-center.object-contain.h-40.w-full(:src="basePath + p.cover_path")
             .card-content.p-5.flex-1.flex.flex-col
               .card-title.text-xl(v-text="p.name")
               .card-desc.my-5.text-base.text-gray-500.leading-6.flex-1(v-text="p.title1")
@@ -107,6 +107,7 @@ export default {
   },
   mounted() {
     this.getSummaryData({ c_id: this.$route.params.ab_id })
+    console.log(this.db)
   },
   methods: {
     ...mapActions(['getSummaryData']),
@@ -156,12 +157,13 @@ export default {
     // width: min(1200px, 100%);
 
     .content {
-      width: 1020px;
+      width: min(1020px, 100%);
     }
 
     .item {
-      width: 300px;
-      margin-left: 40px;
+
+      // width: min(300px, 100%);
+      // margin-left: 40px;
     }
   }
 }
